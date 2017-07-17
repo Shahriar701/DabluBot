2922971d5ad40eaab9cad367d20903d92412b67c
Shahriar701 <shahriar701@gmail.com>
2017-07-18 03:49:38 +0600

    A ChatBot with voice activity

---
+++ app/src/main/java/com/example/dragonsage/dablubot/MainActivity.java
@@ -0,0 +1,321 @@
+package com.example.dragonsage.dablubot;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.Handler;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.alicebot.ab.AIMLProcessor;
+import org.alicebot.ab.Bot;
+import org.alicebot.ab.Chat;
+import org.alicebot.ab.Graphmaster;
+import org.alicebot.ab.MagicBooleans;
+import org.alicebot.ab.MagicStrings;
+import org.alicebot.ab.PCAIMLProcessorExtension;
+import org.alicebot.ab.Timer;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Locale;
+
+import static android.R.id.message;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ListView mListView;
+    private FloatingActionButton mButtonSend;
+    private EditText mEditTextMessage;
+    private ImageView mImageView;
+    public Bot bot;
+    public static Chat chat;
+    private ChatMessage.ChatMessageAdapter mAdapter;
+    public ImageButton buSpeak;
+    public TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+
+                if (status != TextToSpeech.ERROR) {
+                    tts.setLanguage(Locale.US);
+
+                }
+
+            }
+        });
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mButtonSend = (FloatingActionButton) findViewById(R.id.btn_send);
+        mEditTextMessage = (EditText) findViewById(R.id.et_message);
+        mImageView = (ImageView) findViewById(R.id.iv_image);
+        mAdapter = new ChatMessage.ChatMessageAdapter(this, new ArrayList<ChatMessage>());
+        mListView.setAdapter(mAdapter);
+        buSpeak = (ImageButton) findViewById(R.id.buSpeak);
+
+        CheckUserPermsions();
+
+        //chat button
+
+        mButtonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String message = mEditTextMessage.getText().toString();
+                //bot
+                String response = chat.multisentenceRespond(mEditTextMessage.getText().toString());
+                if (TextUtils.isEmpty(message)) {
+                    return;
+                }
+                sendMessage(message);
+                mimicOtherMessage(response);
+                mEditTextMessage.setText("");
+                mListView.setSelection(mAdapter.getCount() - 1);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+                    tts.speak(response, TextToSpeech.QUEUE_FLUSH, null, null);
+
+                } else {
+                    tts.speak(response, TextToSpeech.QUEUE_FLUSH, null);
+                }
+
+            }
+        });
+
+
+        //checking SD card availablility
+
+        boolean a = isSDCARDAvailable();
+
+        //receiving the assets from the app directory
+
+        AssetManager assets = getResources().getAssets();
+        File jayDir = new File(Environment.getExternalStorageDirectory().toString() + "/hari/bots/Hari");
+        boolean b = jayDir.mkdirs();
+        if (jayDir.exists()) {
+
+            //Reading the file
+
+            try {
+                for (String dir : assets.list("Hari")) {
+                    File subdir = new File(jayDir.getPath() + "/" + dir);
+                    boolean subdir_check = subdir.mkdirs();
+                    for (String file : assets.list("Hari/" + dir)) {
+                        File f = new File(jayDir.getPath() + "/" + dir + "/" + file);
+                        if (f.exists()) {
+                            continue;
+                        }
+                        InputStream in = null;
+                        OutputStream out = null;
+                        in = assets.open("Hari/" + dir + "/" + file);
+                        out = new FileOutputStream(jayDir.getPath() + "/" + dir + "/" + file);
+                        //copy file from assets to the mobile's SD card or any secondary memory
+                        copyFile(in, out);
+                        in.close();
+                        in = null;
+                        out.flush();
+                        out.close();
+                        out = null;
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //get the working directory
+
+        MagicStrings.root_path = Environment.getExternalStorageDirectory().toString() + "/hari";
+        System.out.println("Working Directory = " + MagicStrings.root_path);
+        AIMLProcessor.extension = new PCAIMLProcessorExtension();
+
+        //Assign the AIML files to bot for processing
+
+        bot = new Bot("Hari", MagicStrings.root_path, "chat");
+        chat = new Chat(bot);
+        String[] args = null;
+        mainFunction(args);
+
+    }
+
+    private void sendMessage(String message) {
+        ChatMessage chatMessage = new ChatMessage(message, true, false);
+        mAdapter.add(chatMessage);
+
+        //mimicOtherMessage(message);
+    }
+
+    private void mimicOtherMessage(String message) {
+        ChatMessage chatMessage = new ChatMessage(message, false, false);
+        mAdapter.add(chatMessage);
+    }
+
+    private void sendMessage() {
+        ChatMessage chatMessage = new ChatMessage(null, true, true);
+        mAdapter.add(chatMessage);
+        mimicOtherMessage();
+    }
+
+    private void mimicOtherMessage() {
+        ChatMessage chatMessage = new ChatMessage(null, false, true);
+        mAdapter.add(chatMessage);
+    }
+
+    //check SD card availability
+
+    public static boolean isSDCARDAvailable() {
+        return Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED) ? true : false;
+    }
+
+    //copying the file
+    private void copyFile(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int read;
+        while ((read = in.read(buffer)) != -1) {
+            out.write(buffer, 0, read);
+        }
+    }
+    //Request and response of user and the bot
+
+    public static void mainFunction(String[] args) {
+        MagicBooleans.trace_mode = false;
+        System.out.println("trace mode = " + MagicBooleans.trace_mode);
+        Graphmaster.enableShortCuts = true;
+        Timer timer = new Timer();
+        String request = "Hello.";
+        String response = chat.multisentenceRespond(request);
+
+        System.out.println("Human: " + request);
+        System.out.println("Robot: " + response);
+    }
+
+    public void buSpeak(View view) {
+
+        tts.stop();
+
+        startVoiceRecognitionActivity();
+
+    }
+
+
+    private void startVoiceRecognitionActivity() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        //since you only want one, only request 1
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+
+        startActivityForResult(intent, 1234);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        //pull all of the matches
+        ArrayList<String> matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+        String topResult = matches.get(0);
+
+        EditText AutoText = (EditText) findViewById(R.id.et_message);
+        AutoText.setText(topResult);
+
+
+        String message = AutoText.getText().toString();
+        //bot
+        final String response = chat.multisentenceRespond(AutoText.getText().toString());
+        if (TextUtils.isEmpty(response)) {
+            return;
+        }
+        sendMessage(message);
+        mimicOtherMessage(response);
+        mEditTextMessage.setText("");
+        mListView.setSelection(mAdapter.getCount() - 1);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+
+            tts.speak(response, TextToSpeech.QUEUE_FLUSH, null, null);
+
+        } else {
+            tts.speak(response, TextToSpeech.QUEUE_FLUSH, null);
+        }
+
+    }
+
+
+    public void CheckUserPermsions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) !=
+                    PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                                android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
+                return;
+            }
+        }
+
+    }
+
+    //get acces to location permsion
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 123;
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+
+                } else {
+
+                    // Permission Denied
+                    Toast.makeText(this, "denail", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    public void onDestroy() {
+        if (tts != null) {
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+
+    }
+}
